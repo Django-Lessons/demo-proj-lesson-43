06575d931d30604381f886a99b438c6f9eb9c130
Eugen Ciur <eugen@django-lessons.com>
2020-05-21 15:28:21 +0200

    shopix app with its own LoginView

--- land/views.py
+++ land/views.py
@@ -1,12 +1,32 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.shortcuts import render
+from django.utils.http import urlencode
+
 from land.models import Product
+from allauth.account.views import LoginView
+
+
+class ShopixLogin(LoginView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_ids = self.request.GET.getlist('product_ids', [])
+        products = Product.objects.filter(id__in=product_ids).all()
+        context['products'] = products
+
+        return context
 
 
 def buy(request):
+    prod_ids = request.GET.getlist('product_select', [])
+    base_url = reverse('shopix_login')
+    prod_dict = {'product_ids': prod_ids}
+    query_string = urlencode(prod_dict, doseq=True)
+    url = f"{base_url}?{query_string}"
+
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("account_login"))
+        return HttpResponseRedirect(url)
 
     return render(request, 'land/checkout.html')
 
@@ -18,3 +38,5 @@ def index(request):
         'land/index.html',
         {'products': products}
     )
+
+login_view = ShopixLogin.as_view()
--- shopproj/urls.py
+++ shopproj/urls.py
@@ -3,8 +3,11 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from land.views import login_view
+
 urlpatterns = [
     path('', include('land.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accoutns/login/', login_view, name="shopix_login"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
