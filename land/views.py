ad90d00ceeb26c165500c4539a11ec3088b2a4f7
Eugen Ciur <eugen@django-lessons.com>
2020-05-21 08:29:46 +0200

    products and checkout - demo project for Lesson #43

--- land/views.py
+++ land/views.py
@@ -1,5 +1,14 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import reverse
 from django.shortcuts import render
 from land.models import Product
+
+
+def buy(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("account_login"))
+
+    return render(request, 'land/checkout.html')
 
 
 def index(request):
