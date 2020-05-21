2578ca782d4e1a8f306032762aba4a645ae8867a
Eugen Ciur <eugen@django-lessons.com>
2020-05-21 17:32:53 +0200

    final version of shopix demo - Lesson #43

--- land/views.py
+++ land/views.py
@@ -12,7 +12,7 @@ class ShopixLogin(LoginView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         product_ids = self.request.GET.getlist('product_ids', [])
-        products = Product.objects.filter(id__in=product_ids).all()
+        products = Product.objects.filter(id__in=product_ids)
         context['products'] = products
 
         return context
@@ -20,9 +20,9 @@ class ShopixLogin(LoginView):
 
 def buy(request):
     prod_ids = request.GET.getlist('product_select', [])
+
     base_url = reverse('shopix_login')
-    prod_dict = {'product_ids': prod_ids}
-    query_string = urlencode(prod_dict, doseq=True)
+    query_string = urlencode({'product_ids': prod_ids}, doseq=True)
     url = f"{base_url}?{query_string}"
 
     if not request.user.is_authenticated:
@@ -39,4 +39,6 @@ def index(request):
         {'products': products}
     )
 
+
 login_view = ShopixLogin.as_view()
+
--- shopproj/urls.py
+++ shopproj/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
-
 from land.views import login_view
 
 urlpatterns = [
     path('', include('land.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/login/', login_view, name="shopix_login"),
     path('accounts/', include('allauth.urls')),
-    path('accoutns/login/', login_view, name="shopix_login"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
